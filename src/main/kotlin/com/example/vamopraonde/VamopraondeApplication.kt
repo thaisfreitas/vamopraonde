fd63ca65bae146c42112c6dbb75a5967ad1ae93b
Thais Freitas <thaisfreitas31@gmail.com>
2025-02-05 21:21:01 -0300

    add project structure

---
+++ src/main/kotlin/com/example/vamopraonde/VamopraondeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vamopraonde
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VamopraondeApplication
+
+fun main(args: Array<String>) {
+	runApplication<VamopraondeApplication>(*args)
+}
